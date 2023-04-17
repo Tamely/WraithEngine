3815093a319f8e8cb9edfd5cb82283d611c21012
Josh <joshua.audia@gmail.com>
2023-04-16 20:14:46 -0500

    Call the layer's OnDetach before deleting it

--- Wraith/src/Wraith/Core/LayerStack.cpp
+++ Wraith/src/Wraith/Core/LayerStack.cpp
@@ -6,8 +6,10 @@ namespace Wraith {
 	}
 
 	LayerStack::~LayerStack() {
-		for (Layer* layer : m_Layers)
+		for (Layer* layer : m_Layers) {
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer) {
