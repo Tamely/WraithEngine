2a495a50f68ef01147c7a08774f350146147c734
Josh <joshua.audia@gmail.com>
2023-04-16 20:20:44 -0500

    Possible edge case fix

--- Wraith/src/Platform/OpenGL/OpenGLShader.cpp
+++ Wraith/src/Platform/OpenGL/OpenGLShader.cpp
@@ -51,9 +51,11 @@ namespace Wraith {
 			std::string type = source.substr(begin, eol - begin);
 			W_CORE_ASSERT(ShaderTypeFromString(type), "Invalid shader type specified");
 
-			size_t nextLinePos = source.find_first_of("\r\n", eol);
+			size_t nextLinePos = source.find_first_not_of("\r\n", eol);
+			W_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
 			pos = source.find(typeToken, nextLinePos);
-			shaderSources[ShaderTypeFromString(type)] = source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
+
+			shaderSources[ShaderTypeFromString(type)] = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
 		}
 
 		return shaderSources;
