bea9ff0eb3b141708949d4b93e7ffa392acee08d
Josh <joshua.audia@gmail.com>
2023-08-24 12:57:05 -0500

    Properties panel

--- Wraith-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Wraith-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -1,6 +1,7 @@
 #include "SceneHierarchyPanel.h"
 
-#include "imgui.h"
+#include <ImGui-Premake/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Wraith/Scene/Components.h"
 
@@ -21,6 +22,16 @@ namespace Wraith {
 			DrawEntityNode(entity);
 		});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered()) {
+			m_SelectionContext = {};
+		}
+
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext) {
+			DrawComponents(m_SelectionContext);
+		}
 		ImGui::End();
 	}
 
@@ -35,6 +46,29 @@ namespace Wraith {
 
 		if (opened) {
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity) {
+		if (entity.HasComponent<TagComponent>()) {
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer)); // Null terminate the "string"
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer))) {
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>()) {
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform")) {
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 }
