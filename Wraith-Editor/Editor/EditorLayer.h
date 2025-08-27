06a6c39654405256949b92f1d492b7f59e769bc8
Josh <joshua.audia@gmail.com>
2025-08-27 14:00:03 -0500

    [WRAITH-40] Dragging And Dropping to Open Scenes

--- Wraith-Editor/Editor/EditorLayer.h
+++ Wraith-Editor/Editor/EditorLayer.h
@@ -24,6 +24,7 @@ namespace Wraith {
 
 		void NewScene();
 		void OpenScene();
+		void OpenScene(const std::filesystem::path& filePath);
 		void SaveSceneAs();
 	private:
 		OrthographicCameraController m_CameraController;
