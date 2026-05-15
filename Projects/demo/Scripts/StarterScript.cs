96802a17d2ea41b53852881e599279c0d96bb26e
Tamely <joshua.audia@gmail.com>
2026-05-15 01:58:08 -0500

    More realistic demo level

---
+++ Projects/demo/Scripts/StarterScript.cs
@@ -0,0 +1,14 @@
+using WraithEngine;
+
+namespace Demo.Scripts;
+
+public class StarterScript : Script
+{
+    public override void OnCreate()
+    {
+    }
+
+    public override void OnTick(float dt)
+    {
+    }
+}
